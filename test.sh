94d933e673b60ff0a05dcc214fd5079f5fbfea32
Tired Blue Whale <tired.blue.whale@gmail.com>
2026-04-14 13:21:15 +0200

    Add single node monitor

--- test.sh
+++ test.sh
@@ -7,4 +7,8 @@ docker exec -it reg_node_0 ping6 -c 1 fd00:2:3::2
 echo "Node 3 -> Node 0"
 docker exec -it dtn_node_3 ping6 -c 1 fd00:0:1::1
 
+echo "Node 1 -> Node 3"
 docker exec -it dtn_node_1 ping6 -c 1 fd00:2:3::2
+
+echo "UDP Node 0 -> Node 3"
+docker exec -it reg_node_0 sh -c "echo 'Hello from plain eagle' | nc -u -w 1 fd00:2:3::2 8080"
